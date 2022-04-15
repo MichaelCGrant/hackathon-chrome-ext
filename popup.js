45b4a8f03c69fb5d8d551b32a91a9ed67442cfe4
MichaelCGrant <MichaelCGrant@gmail.com>
2022-04-14 21:20:10 -0500

    window changes
    Co-authored-by: MarkA772 <MarkA772@users.noreply.github.com>

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
 const button = document.querySelector('#start-button');
 
 button.onclick = () => f();
+
+const tab_title = '';
+function display_h1 (results){
+  h1=results;
+  document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
+}
+chrome.tabs.query({active: true}, function(tabs) {
+  const tab = tabs[0];
+  tab_title = tab.title;
+  chrome.tabs.executeScript(tab.id, {
+    code: 'document.querySelector("h1").textContent'
+  }, display_h1);
+});
